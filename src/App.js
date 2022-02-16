a268888aa55ae91d53914dac2219f7787b0b719c
brandon-earls <brandon.samuel@earls.ca>
2022-02-15 16:03:23 -0800

    Created the homepage component and added scss to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+
+import HomePage from './components/home-page/homepage.component';
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
