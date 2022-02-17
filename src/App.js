fd58280dcb30436a2de86c7cf0fc633faa01d11d
brandon-earls <brandon.samuel@earls.ca>
2022-02-16 16:14:23 -0800

    Added react-router-dom for routing. Created first route page for hats page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/home-page/homepage.component';
+import HatsPage from './pages/hats-page/hats-page.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path={'/'} component={HomePage} />
+        <Route path={'/shop/hats'} component={HatsPage} />
+      </Switch>
     </div>
   );
 }
